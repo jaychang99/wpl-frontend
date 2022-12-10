57539cb1504df84881f492db9f8f39f60bf1490c
jaychang99 <jaychang99@gmail.com>
2022-12-11 01:32:41 +0900

    feat: authFetcher, fetcher 등 GET 함수 추가

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { serverAxios } from "utils/commonAxios";
+import { getCookie } from "utils/cookies";
+
+class CustomError extends Error {
+  status: number;
+
+  constructor(status: number, message?: string) {
+    super(message);
+    this.status = status;
+  }
+}
+
+export async function fetcher<T>(url: string) {
+  return (await serverAxios.get<T>(url)).data;
+}
+
+export async function authFetcher<T>(url: string, cookies?: string) {
+  const token = getCookie("wts_web_token", cookies);
+  console.log("authFetcher TOKEN", token);
+  if (!token || token === "") {
+    const error = new CustomError(401, "Not Authorized");
+    throw error;
+  }
+
+  return (
+    await serverAxios.get<T>(url, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+  ).data;
+}
