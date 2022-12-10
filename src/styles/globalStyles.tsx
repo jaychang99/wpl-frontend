03064ca1e4eb8cff3799e978578e89767fdd331d
jaychang99 <jaychang99@gmail.com>
2022-12-10 14:55:12 +0900

    feat: GlobalStyles 설정 완료

---
+++ src/styles/globalStyles.tsx
@@ -0,0 +1,41 @@
+import { css, Global, Theme, useTheme } from "@emotion/react";
+import emotionReset from "emotion-reset";
+
+const globalStyles = (theme: Theme) => css`
+  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-std.css");
+
+  ${emotionReset}
+
+  html, body {
+    min-height: 100%;
+    word-break: keep-all;
+    line-height: 1.5;
+    letter-spacing: -0.005em;
+
+    font-size: 16px;
+    font-family: "Pretendard Std Variable", "Pretendard Std", Pretendard,
+      -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue",
+      "Segoe UI", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+      sans-serif;
+  }
+
+  body {
+    background-color: white;
+  }
+
+  button,
+  input,
+  textarea {
+    font: inherit;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export function GlobalStyles() {
+  const theme = useTheme();
+
+  return <Global styles={globalStyles(theme)} />;
+}
