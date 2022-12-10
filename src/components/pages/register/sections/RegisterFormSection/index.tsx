ebd7019efe5ceb000fbed8fdbad1aae3dd101019
jaychang99 <jaychang99@gmail.com>
2022-12-10 20:03:27 +0900

    feat: registerForm 에서 email input 에 type: email 부여 및 API 연결 위한 name attribute 도 부여

--- src/components/pages/register/sections/RegisterFormSection/index.tsx
+++ src/components/pages/register/sections/RegisterFormSection/index.tsx
@@ -6,10 +6,16 @@ import { StyledRegisterFormSection } from "components/pages/register/sections/Re
 function RegisterFormSection() {
   return (
     <StyledRegisterFormSection>
-      <Input label="email" placeholder="Please input email" />
+      <Input
+        type="email"
+        name="email"
+        label="email"
+        placeholder="Please input email"
+      />
       <Input
         type="password"
         label="password"
+        name="password"
         placeholder="Please input password"
       />
       <Input
@@ -17,7 +23,11 @@ function RegisterFormSection() {
         label="confirm password"
         placeholder="Please confirm password"
       />
-      <Input label="nickname" placeholder="Please input nickname" />
+      <Input
+        name="nickname"
+        label="nickname"
+        placeholder="Please input nickname"
+      />
       <Button
         css={css`
           margin-top: 30px;
