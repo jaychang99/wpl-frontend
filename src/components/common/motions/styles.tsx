fbddac3d9c7f63c3843b4f506de293b452ce7805
jaychang99 <jaychang99@gmail.com>
2022-12-11 21:26:42 +0900

    fix: bottomsheet z-index 10000 으로 변경

--- src/components/common/motions/styles.tsx
+++ src/components/common/motions/styles.tsx
@@ -2,6 +2,6 @@ import styled from "@emotion/styled";
 import { motion } from "framer-motion";
 
 export const DefaultMotionDiv = styled(motion.div)`
-  z-index: 10;
+  z-index: 10000;
   position: relative;
 `;
