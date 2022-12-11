e8f216c9eb00d6e43094239806a7d6c4a45f6653
jaychang99 <jaychang99@gmail.com>
2022-12-11 19:52:34 +0900

    feat: 로그인 된 상태에서 회원가입 페이지 접근 차단

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,7 +2,9 @@ import PageLayout from "components/common/PageLayout";
 import RegisterFooterSection from "components/pages/register/sections/RegisterFooterSection";
 import RegisterFormSection from "components/pages/register/sections/RegisterFormSection";
 import RegisterHeaderSection from "components/pages/register/sections/RegisterHeaderSection";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { getServerAxiosErrorInfo } from "utils/commonAxios";
+import getNavbarServerSideProps from "utils/getServerSideProps";
 
 const Register: NextPage = () => {
   return (
@@ -12,6 +14,44 @@ const Register: NextPage = () => {
       <RegisterFooterSection />
     </PageLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const nicknameObject = await getNavbarServerSideProps({ ...(ctx as any) });
+  const nickname = nicknameObject.props.nickname;
+
+  const requestURL = "places";
+  ctx.res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+  const config = {};
+  try {
+    if (nickname) {
+      // if logged in
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/",
+        },
+        props: {},
+      };
+    } else {
+      // not logged in
+      return {
+        props: {},
+      };
+    }
+  } catch (err) {
+    const { title, statusCode } = getServerAxiosErrorInfo(err);
+
+    return {
+      props: {
+        error: { title, statusCode },
+        data: {},
+      },
+    };
+  }
 };
 
 export default Register;
