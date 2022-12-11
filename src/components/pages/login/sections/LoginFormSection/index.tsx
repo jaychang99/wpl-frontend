ea11fb432262d2714d46aee7987782fd00d9205f
jaychang99 <jaychang99@gmail.com>
2022-12-11 19:20:28 +0900

    feat: 로그인 페이지에서 예외 처리 완료

--- src/components/pages/login/sections/LoginFormSection/index.tsx
+++ src/components/pages/login/sections/LoginFormSection/index.tsx
@@ -1,14 +1,21 @@
 import { css } from "@emotion/react";
 import Button from "components/common/Button";
 import Input from "components/common/Input";
+import Modal from "components/common/Modal";
 import { StyledLoginFormSection } from "components/pages/login/sections/LoginFormSection/styles";
 import router from "next/router";
-import { FormEvent, useCallback } from "react";
+import { FormEvent, useCallback, useState } from "react";
 import { LoginInputFields } from "types/inputs";
 import { serverAxios } from "utils/commonAxios";
 import { setCookie } from "utils/cookies";
 
 function LoginFormSection() {
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    title: "",
+    content: "",
+  });
+
   const handleSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     async function handleRegister() {
@@ -27,44 +34,72 @@ function LoginFormSection() {
           password: formElements?.password.value,
         };
 
-        serverAxios.post(requestURL, body, config).then(function (response) {
-          // on success of POST request
-          // on login success
-          router.push("/");
-          setCookie("wts_web_token", response.data.access_token, 1);
-        });
+        serverAxios
+          .post(requestURL, body, config)
+          .then(function (response) {
+            // on success of POST request
+            // on login success
+            router.push("/");
+            setCookie("wts_web_token", response.data.access_token, 1);
+          })
+          .catch((error) => {
+            setModalContent({
+              title: "Login Failed",
+              content: "Invalid credentials. Please try again ",
+            });
+            setShowModal(true);
+            return;
+          });
       } catch (e) {
-        console.log(e);
+        // if request itself fails
+        setModalContent({
+          title: "Sign in Request Failed",
+          content: "Sign in Request Failed. Please contact the site owner",
+        });
+        setShowModal(true);
       }
     }
     handleRegister();
   }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <StyledLoginFormSection>
-        <Input
-          name="email"
-          type="email"
-          label="email"
-          placeholder="Please input email"
-        />
-        <Input
-          name="password"
-          type="password"
-          label="password"
-          placeholder="Please input password"
-        />
-        <Button
-          type="submit"
-          css={css`
-            margin-top: 30px;
-          `}
-        >
-          Login
-        </Button>
-      </StyledLoginFormSection>
-    </form>
+    <>
+      <Modal
+        open={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+        title={modalContent.title}
+        buttons={<></>}
+        withCloseButton
+      >
+        {modalContent.content}
+      </Modal>
+      <form onSubmit={handleSubmit}>
+        <StyledLoginFormSection>
+          <Input
+            name="email"
+            type="email"
+            label="email"
+            placeholder="Please input email"
+          />
+          <Input
+            name="password"
+            type="password"
+            label="password"
+            placeholder="Please input password"
+          />
+          <Button
+            type="submit"
+            css={css`
+              margin-top: 30px;
+            `}
+          >
+            Login
+          </Button>
+        </StyledLoginFormSection>
+      </form>
+    </>
   );
 }
 
