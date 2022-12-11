b597fa853d6a29b9bf6948690158806ab13fe284
jaychang99 <jaychang99@gmail.com>
2022-12-11 19:46:56 +0900

    feat: /report 페이지에서는 로그인 안 돼 있으면 로그인 페이지로 리다이렉팅 되도록 변경

--- src/pages/report.tsx
+++ src/pages/report.tsx
@@ -2,8 +2,9 @@ import PageLayout from "components/common/PageLayout";
 import ReportFooterSection from "components/pages/report/sections/ReportFooterSection";
 import ReportFormSection from "components/pages/report/sections/ReportFormSection";
 import ReportHeaderSection from "components/pages/report/sections/ReportHeaderSection";
-import type { NextPage } from "next";
-import getServerSideProps from "utils/getServerSideProps";
+import type { GetServerSideProps, NextPage } from "next";
+import { getServerAxiosErrorInfo, serverAxios } from "utils/commonAxios";
+import getNavbarServerSideProps from "utils/getServerSideProps";
 
 const Report: NextPage = () => {
   return (
@@ -15,6 +16,43 @@ const Report: NextPage = () => {
   );
 };
 
-export { getServerSideProps };
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const nicknameObject = await getNavbarServerSideProps({ ...(ctx as any) });
+  const nickname = nicknameObject.props.nickname;
+
+  const requestURL = "places";
+  ctx.res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+  const config = {};
+  try {
+    if (nickname) {
+      return {
+        props: {
+          nickname,
+        },
+      };
+    } else {
+      // not logged in
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/login",
+        },
+        props: {},
+      };
+    }
+  } catch (err) {
+    const { title, statusCode } = getServerAxiosErrorInfo(err);
+
+    return {
+      props: {
+        error: { title, statusCode },
+        data: {},
+      },
+    };
+  }
+};
 
 export default Report;
