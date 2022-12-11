1511123488934a7a7f353084fa07145ab61ad568
jaychang99 <jaychang99@gmail.com>
2022-12-12 04:02:08 +0900

    feat: https 사이트에서도 http 요청 할 수 있도록 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 function MyDocument() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body>
         <Main />
         {/* Modal -> regardless of where it's called, always on top */}
