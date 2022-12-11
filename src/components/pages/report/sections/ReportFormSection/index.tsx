2f1917fd20f9f0cebf0c4176c0100ee4cd6b5005
jaychang99 <jaychang99@gmail.com>
2022-12-11 19:36:07 +0900

    feat: contribute 에서 재확인 절차 추가

--- src/components/pages/report/sections/ReportFormSection/index.tsx
+++ src/components/pages/report/sections/ReportFormSection/index.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/react";
 import Button from "components/common/Button";
 import Dropdown from "components/common/Dropdown";
+import Modal from "components/common/Modal";
 import { StyledReportFormSection } from "components/pages/report/sections/ReportFormSection/styles";
 import { CROWDEDNESS } from "constants/crowdedness";
 import usePlaces from "hooks/usePlaces";
@@ -17,6 +18,16 @@ function ReportFormSection() {
   const [selectedPlace, setSelectedPlace] = useState("");
   const [selectedCrowdedness, setselectedCrowdedness] = useState("");
 
+  // modal related
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    title: "",
+    content: "",
+  });
+
+  // to check if first time clicking on generate report
+  const [isSecondTime, setIsSecondTime] = useState(false);
+
   // fetch places info for dropdown
   const { placeNames, placeIds, error, mutate } = usePlaces();
 
@@ -24,6 +35,17 @@ function ReportFormSection() {
     (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       async function handleReport() {
+        if (!isSecondTime) {
+          setModalContent({
+            title: "Confirm Submission",
+            content:
+              "Your submission is not yet Registered. Thank you for attempting to contribute to our service. Because this service is community-based, please make sure once again your estimation is correct. If you do think your estimation is correct, please close this dialoge box and click the button once again!",
+          });
+          setShowModal(true);
+          setIsSecondTime(true);
+          return;
+        }
+
         const requestURL = "/places";
 
         try {
@@ -44,37 +66,50 @@ function ReportFormSection() {
       }
       handleReport();
     },
-    [placeIds, placeNames, selectedCrowdedness, selectedPlace]
+    [isSecondTime, placeIds, placeNames, selectedCrowdedness, selectedPlace]
   );
 
   return (
-    <form onSubmit={handleSubmit}>
-      <StyledReportFormSection>
-        <Dropdown
-          stackingorder={1}
-          label="place"
-          placeholder="Select Place"
-          list={placeNames}
-          setItem={setSelectedPlace}
-        />
-        <Dropdown
-          stackingorder={2}
-          label="crowdedness"
-          placeholder="Select Crowdedness"
-          list={CROWDEDNESS}
-          setItem={setselectedCrowdedness}
-        />
+    <>
+      <Modal
+        open={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+        title={modalContent.title}
+        buttons={<></>}
+        withCloseButton
+      >
+        {modalContent.content}
+      </Modal>
+      <form onSubmit={handleSubmit}>
+        <StyledReportFormSection>
+          <Dropdown
+            stackingorder={1}
+            label="place"
+            placeholder="Select Place"
+            list={placeNames}
+            setItem={setSelectedPlace}
+          />
+          <Dropdown
+            stackingorder={2}
+            label="crowdedness"
+            placeholder="Select Crowdedness"
+            list={CROWDEDNESS}
+            setItem={setselectedCrowdedness}
+          />
 
-        <Button
-          type="submit"
-          css={css`
-            margin-top: 30px;
-          `}
-        >
-          Make mine the latest report!
-        </Button>
-      </StyledReportFormSection>
-    </form>
+          <Button
+            type="submit"
+            css={css`
+              margin-top: 30px;
+            `}
+          >
+            Make mine the latest report!
+          </Button>
+        </StyledReportFormSection>
+      </form>
+    </>
   );
 }
 
